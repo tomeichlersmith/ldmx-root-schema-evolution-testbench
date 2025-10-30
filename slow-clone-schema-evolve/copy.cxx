713d3d06fd41a81b9b558650691a2819c5da66ce
tomeichlersmith <eichl008@umn.edu>
2025-10-30 09:45:02 -0500

    i think i got something working with my own CloneTree

--- slow-clone-schema-evolve/copy.cxx
+++ slow-clone-schema-evolve/copy.cxx
@@ -18,10 +18,42 @@ int main(int nargs, char** argv) {
   TFile o{argv[2], "recreate"};
 
   input_tree->GetEntry(0);
+
+  /* fine, i'll write my own CloneTree
+   */
+  TTree* output_tree = new TTree("tree","tree");
+  auto input_branches = input_tree->GetListOfBranches();
+  for (std::size_t i_br{0}; i_br < input_branches->GetEntriesFast(); i_br++) {
+    auto br{dynamic_cast<TBranchElement*>(input_branches->UncheckedAt(i_br))};
+
+    // check if copying
+    // e.g. with GetBranchStatus
+
+    output_tree->Branch(br->GetName(), br->GetClassName(), br->GetObject());
+  }
+
+  output_tree->Print();
+
+  /* using CloneTree
   TTree* output_tree = input_tree->CloneTree(0);
+  */
+
   Header* h_ptr = nullptr; //new Header;
   input_tree->SetBranchAddress("header", &h_ptr);
   output_tree->SetBranchAddress("header", &h_ptr);
+
+  /* unset kDeleteObject
+  auto input_br = input_tree->GetBranch("header");
+  // TBranchElement::EStatusBits::kDeleteObject
+  // is protected so I manually copy the index
+  input_br->SetBit((1ULL << (16)), false);
+  input_br->SetObject(h_ptr);
+
+  auto output_br = output_tree->GetBranch("header");
+  output_br->SetBit((1ULL << (16)), false);
+  dynamic_cast<TBranchElement*>(output_br)->ResetDeleteObject();
+  output_br->SetObject(h_ptr);
+  */
 
   for (std::size_t i{0}; i < input_tree->GetEntriesFast(); i++) {
     input_tree->GetEntry(i);
