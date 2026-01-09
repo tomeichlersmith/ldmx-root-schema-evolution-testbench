e8151f8b67bbb114e879823ff1e839e2384fd48a
tomeichlersmith <eichl008@umn.edu>
2026-01-09 09:43:09 -0600

    try avoiding TTreeReader

--- forum/schema-evolve-std-map-value/read.cxx
+++ forum/schema-evolve-std-map-value/read.cxx
@@ -8,11 +8,13 @@
 
 int main(int nargs, char** argv) {
   TFile f{FILENAME};
-  TTreeReader tree("tree", &f);
-  TTreeReaderValue<std::map<int, Header>> header_map(tree, "header_map");
-  while (tree.Next()) {
+  auto tree = f.Get<TTree>("tree");
+  std::map<int, Header> header_map;
+  tree->GetBranch("header_map")->SetObject(&header_map);
+  for (int i_entry{0}; i_entry < tree->GetEntriesFast(); i_entry++) {
+    tree->GetEntry(i_entry);
     std::cout << "header_map: {";
-    for (const auto& [i, h] : *(header_map.Get())) {
+    for (const auto& [i, h] : header_map) {
       std::cout << " " << i << ": { run: " << h.getRun() << ", event: " << h.getEvent() << "}";
     }
     std::cout << " }" << std::endl;
